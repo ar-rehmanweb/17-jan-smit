a780fcd312f465520f6c8e8b0c107dff130ea5fc
ar-rehman-web <abdurrehmanirfan94@gmail.com>
2026-01-17 09:52:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let obj = {
+    name: "hasan",   /*json mai key aur value dono string mai hoti hai*/
+    email: "coder@gmail.com",
+    password: "sbfduwd9ufwifh"
+}
+
+
+// .parse ka method string ko orgnial form mai lata hai
+
+
+
+// const varname = () => {}
